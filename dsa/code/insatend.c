ab38819b416dddfa6bd1012080df532c30878e8d
Peaxmoon <sujjalboi09@gmail.com>
2024-03-19 10:05:12 +0545

    basic code

---
+++ dsa/code/insatend.c
@@ -0,0 +1,16 @@
+int isFull(struct list*list)
+
+
+int insertAtEnd(struct list*, struct product)
+
+void display(struct list*list){
+    int i;
+    if(isEmpty(lst))
+    printf("List is empty. ");
+    else{
+        printf("List contains : ");
+        for(i=0;i<lst->count;i++){
+            printf("pName,lst->");
+        }
+    }
+}
--- dsa/hellllo.java
+++ dsa/hellllo.java
@@ -1,4 +1,5 @@
-public class HelloWorld {
+package dsa;
+public class Helllo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
